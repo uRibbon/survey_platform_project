8879d1e8e13ce8ac6d64ed1df1dace2f68373e1a
서유리 <party22g@naver.com>
2022-10-28 19:37:51 +0900

    ad chart shape change

--- src/views/analysis/ad_survey_analysis.js
+++ src/views/analysis/ad_survey_analysis.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { CCard, CCardBody, CCol, CCardHeader, CRow } from '@coreui/react'
-import { CChartBar, CChartPie, CChartLine } from '@coreui/react-chartjs'
+import { CChartBar, CChartLine, CChartDoughnut, } from '@coreui/react-chartjs'
 
 const Charts = () => {
-  //const random = () => Math.round(Math.random() * 100)
-
   return (
     <CRow>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>참여자 연령대</CCardHeader>
+          <CCardHeader>월별 회원가입</CCardHeader>
           <CCardBody>
             <CChartBar
               data={{
-                labels: ['10대', '20대', '30대', '40대', '50대', '60대'],
+                labels: [
+                  '1월',
+                  '2월',
+                  '3월',
+                  '4월',
+                  '5월',
+                  '6월',
+                  '7월',
+                  '8월',
+                  '9월',
+                  '10월',
+                  '11월',
+                  '12월',],
                 datasets: [
                   {
-                    label: '참여자 연령대',
-                    backgroundColor: '#FFC314',
-                    data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+                    label: '회원가입 수',
+                    backgroundColor: '#D3D3D3',
+                    data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 70, 35, 58],
                   },
                 ],
               }}
@@ -29,26 +39,32 @@ const Charts = () => {
       </CCol>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>참여자 설문 응답 시간대</CCardHeader>
+          <CCardHeader>월별 설문 등록</CCardHeader>
           <CCardBody>
             <CChartLine
               data={{
                 labels: [
-                  '00:00~04:00',
-                  '04:00~08:00',
-                  '08:00~12:00',
-                  '12:00~16:00',
-                  '16:00~20:00',
-                  '20:00~24:00',
+                  '1월',
+                  '2월',
+                  '3월',
+                  '4월',
+                  '5월',
+                  '6월',
+                  '7월',
+                  '8월',
+                  '9월',
+                  '10월',
+                  '11월',
+                  '12월',
                 ],
                 datasets: [
                   {
-                    label: '시간별 추이',
+                    label: '설문 등록 개수',
                     backgroundColor: 'rgba(151, 187, 205, 0.2)',
                     borderColor: 'rgba(151, 187, 205, 1)',
                     pointBackgroundColor: 'rgba(151, 187, 205, 1)',
                     pointBorderColor: '#fff',
-                    data: [2, 3, 5, 40, 48, 96],
+                    data: [50, 68, 38, 87, 39, 49, 60, 40, 38, 68, 47, 38],
                   },
                 ],
               }}
@@ -58,16 +74,33 @@ const Charts = () => {
       </CCol>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>참여자 남여 비율</CCardHeader>
+          <CCardHeader>카테고리별 설문 개수</CCardHeader>
           <CCardBody>
-            <CChartPie
+            <CChartDoughnut
               data={{
-                labels: ['남자', '여자'],
+                labels: ['학교', '취미', '연애', '기업'],
                 datasets: [
                   {
-                    data: [60, 40],
-                    backgroundColor: ['#CCEEFF', '#FFD2D7'],
-                    hoverBackgroundColor: ['#CCEEFF', '#FFD2D7'],
+                    backgroundColor: ['#FFD700', '#90EE90', '#AFEEEE', '#DDA0DD'],
+                    data: [40, 20, 50, 30],
+                  },
+                ],
+              }}
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <CCol xs={6}>
+        <CCard className="mb-4">
+          <CCardHeader>카테고리별 설문 조회수</CCardHeader>
+          <CCardBody>
+            <CChartDoughnut
+              data={{
+                labels: ['학교', '취미', '연애', '기업'],
+                datasets: [
+                  {
+                    backgroundColor: ['#FFD700', '#90EE90', '#AFEEEE', '#DDA0DD'],
+                    data: [36, 57, 64, 47],
                   },
                 ],
               }}
