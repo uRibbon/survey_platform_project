d701202aac926a826dd427d05ea0ebe7b2c16210
서유리 <party22g@naver.com>
2022-10-28 20:38:17 +0900

    user survey 항목 추가

--- src/views/analysis/user_survey_analysis.js
+++ src/views/analysis/user_survey_analysis.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CCard, CCardBody, CCol, CCardHeader, CRow } from '@coreui/react'
-import { CChartBar, CChartPie, CChartLine } from '@coreui/react-chartjs'
+import { CChartBar, CChartPie, CChartLine, CChartDoughnut } from '@coreui/react-chartjs'
 
 const Charts = () => {
   //const random = () => Math.round(Math.random() * 100)
@@ -9,16 +9,16 @@ const Charts = () => {
     <CRow>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>참여자 연령대</CCardHeader>
+          <CCardHeader>설문 응답자 연령대</CCardHeader>
           <CCardBody>
             <CChartBar
               data={{
                 labels: ['10대', '20대', '30대', '40대', '50대', '60대'],
                 datasets: [
                   {
-                    label: '참여자 연령대',
-                    backgroundColor: '#FFC314',
-                    data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+                    label: '연령대별 추이',
+                    backgroundColor: '#cfd0fe',
+                    data: [40, 20, 12, 39, 10, 65],
                   },
                 ],
               }}
@@ -29,27 +29,20 @@ const Charts = () => {
       </CCol>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>사용자 설문 응답 시간대</CCardHeader>
+          <CCardHeader>설문 응답자 직업</CCardHeader>
           <CCardBody>
             <CChartLine
               data={{
-                labels: [
-                  '00:00~04:00',
-                  '04:00~08:00',
-                  '08:00~12:00',
-                  '12:00~16:00',
-                  '16:00~20:00',
-                  '20:00~24:00',
-                ],
+                labels: ['학생', '직장인', '자영업자', '주부', '군인'],
                 datasets: [
                   {
-                    label: '시간별 추이',
-                    backgroundColor: 'rgba(151, 187, 205, 0.2)',
-                    borderColor: 'rgba(151, 187, 205, 1)',
-                    pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+                    label: '직업별 추이',
+                    backgroundColor: 'rgba(220, 220, 220, 0.2)',
+                    borderColor: 'rgba(220, 220, 220, 1)',
+                    pointBackgroundColor: 'rgba(220, 220, 220, 1)',
                     pointBorderColor: '#fff',
-                    data: [2, 3, 5, 40, 48, 96],
-                  },
+                    data: [50, 40, 35, 30, 5],
+                  }
                 ],
               }}
             />
@@ -58,7 +51,7 @@ const Charts = () => {
       </CCol>
       <CCol xs={6}>
         <CCard className="mb-4">
-          <CCardHeader>참여자 남여 비율</CCardHeader>
+          <CCardHeader>설문 응답자 남녀 비율</CCardHeader>
           <CCardBody>
             <CChartPie
               data={{
@@ -68,6 +61,31 @@ const Charts = () => {
                     data: [60, 40],
                     backgroundColor: ['#CCEEFF', '#FFD2D7'],
                     hoverBackgroundColor: ['#CCEEFF', '#FFD2D7'],
+                  },
+                ],
+              }}
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <CCol xs={6}>
+        <CCard className="mb-4">
+          <CCardHeader>설문 응답 시간대</CCardHeader>
+          <CCardBody>
+            <CChartDoughnut
+              data={{
+                labels: [
+                  '00시~04시',
+                  '04시~08시',
+                  '08시~12시',
+                  '12시~16시',
+                  '16시~20시',
+                  '20시~24시',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: ['#FFD700', '#90EE90', '#AFEEEE', '#DDA0DD', '#FFA2AD', '#BEBEBE'],
+                    data: [5, 20, 50, 40, 50, 60],
                   },
                 ],
               }}
