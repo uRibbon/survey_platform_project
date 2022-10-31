7da0ebb437123540e87323612c17d93f28704bec
solbiko <gsb0904@hanmail.net>
2022-11-01 00:21:34 +0900

    first commit

---
+++ src/main/java/com/cloud/common/controller/commonServiceController.java
@@ -0,0 +1,16 @@
+package com.cloud.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="v1/common")
+public class commonServiceController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "common controller response";
+    }
+}
+
