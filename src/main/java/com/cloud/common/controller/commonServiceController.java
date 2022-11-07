1c298397efd32042a85a9d462507e9dc3e5de80a
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-08 02:09:11 +0900

    모든 menu list 반환하는 rest api 구현(dto버전, entity버전)

--- src/main/java/com/cloud/common/controller/commonServiceController.java
+++ src/main/java/com/cloud/common/controller/commonServiceController.java
@@ -1,15 +1,39 @@
 package com.cloud.common.controller;
 
+import com.cloud.common.dto.MenuDTO;
+import com.cloud.common.entity.Menu;
+import com.cloud.common.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value="v1/common")
 public class commonServiceController {
+    @Autowired
+    private MenuService menuService;
+
     @GetMapping("/test")
     public String test() {
         return "Common서버로부터 응답.";
+    }
+
+    @GetMapping("/menuDTO")
+    public List<MenuDTO> getAllMenuDTO(Model model) {
+        List<MenuDTO> allMenuDTOList = menuService.getAllMenuDTOList();
+        model.addAttribute("allMenuDTOList", allMenuDTOList);
+        return allMenuDTOList;
+    }
+
+    @GetMapping("/menu")
+    public List<Menu> getAllMenu(Model model) {
+        List<Menu> allMenuList = menuService.getAllMenuList();
+        model.addAttribute("allMenuList", allMenuList);
+        return allMenuList;
     }
 }
 
