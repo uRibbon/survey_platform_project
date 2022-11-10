feb1d7fccd95b20b9da89a54fc515f755fbc2020
Youngjoo Oh <dhdudwn95@hansung.ac.kr>
2022-11-10 11:46:58 +0900

    navigation 서버로부터 불러와서 뿌리는 알고리즘 작성

--- src/_nav.js
+++ src/_nav.js
@@ -1,15 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CIcon from '@coreui/icons-react'
 import {
+  cilPrint,
   cilSpreadsheet,
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+
+const Test=()=>
+{let dummy_data = [
+  {
+    "menuId": 1,
+    "menuName": "survey",
+    "menuUrl": null,
+    "menuIcon": null,
+    "menuList": [
+      {
+        "menuId": 101,
+        "menuName": "search",
+        "menuUrl": null,
+        "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+        "menuList": [
+          {
+            "menuId": 10101,
+            "menuName": "설문 검색",
+            "menuUrl": "/survey/search",
+            "menuIcon": null,
+            "menuList": []
+          }
+        ]
+      },
+      {
+        "menuId": 102,
+        "menuName": "create",
+        "menuUrl": null,
+        "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+        "menuList": [
+          {
+            "menuId": 10201,
+            "menuName": "설문 생성",
+            "menuUrl": "/survey",
+            "menuIcon": null,
+            "menuList": []
+          }
+        ]
+      },
+      {
+        "menuId": 103,
+        "menuName": "list",
+        "menuUrl": null,
+        "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+        "menuList": [
+          {
+            "menuId": 10301,
+            "menuName": "참여 목록",
+            "menuUrl": "/survey/prtcpList",
+            "menuIcon": null,
+            "menuList": []
+          },
+          {
+            "menuId": 10302,
+            "menuName": "생성 목록",
+            "menuUrl": "/survey/makeList",
+            "menuIcon": null,
+            "menuList": []
+          }
+        ]
+      }
+    ]
+  }
+]
 
 let _nav = [
   {
     component: CNavTitle,
     name: 'Survey',
   },
+
+
   {
     component: CNavGroup,
     name: 'Search',
@@ -23,6 +91,8 @@ let _nav = [
       },
     ],
   },
+
+
   {
     component: CNavGroup,
     name: 'Create',
@@ -156,4 +226,33 @@ let _nav = [
   },
 ]
 
-export default _nav
+console.log(typeof(dummy_data))
+console.log(dummy_data)
+// const [nav, setNav] = useState([])
+
+let new_nav = [
+  
+  dummy_data.map((large) => {
+
+      let L = {
+        component: CNavTitle,
+        name: large.menuName,
+      }
+      // setNav([...nav, L])
+    
+      // console.log('대분류만 추가함: ',nav)
+
+      large.menuList.map((middle) =>{
+        console.log('중분류: ', middle.menuName)
+        console.log('소분류들: ', middle.menuList)
+      }
+      )
+  })
+]
+
+return(
+  _nav
+)
+}
+
+export default Test
--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,24 +1,283 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
 import { AppSidebarNav } from './AppSidebarNav'
-
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
-
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
+import {
+  cilSpreadsheet,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 
-// sidebar nav config
-import navigation from '../_nav'
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
+  const [navigation, setNavigation] = useState([])
+
+  let _nav = [
+    {
+      component: CNavTitle,
+      name: 'Survey',
+    },
+    {
+      component: CNavGroup,
+      name: 'Search',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '설문검색',
+          to: '/survey/search',
+        },
+      ],
+    },
+    {
+      component: CNavGroup,
+      name: 'Create',
+      to: '/survey',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '설문생성',
+          to: '/survey',
+        },
+        {
+          component: CNavItem,
+          name: '설문참여',
+          to: '/survey/ParticipateSurvey',
+        },
+        {
+          component: CNavItem,
+          name: '설문수정',
+          to: '/survey/ModifySurvey',
+        },
+      ],
+    },
+    {
+      component: CNavGroup,
+      name: 'List',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '참여목록',
+          to: '/survey/prtcpList',
+        },
+        {
+          component: CNavItem,
+          name: '작성목록',
+          to: '/survey/makeList',
+        },
+      ],
+    },
+    {
+      component: CNavTitle,
+      name: 'Group',
+    },
+    {
+      component: CNavGroup,
+      name: 'List',
+      to: '/group',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '그룹 목록',
+          to: '/group/list',
+        },
+      ],
+    },
+    {
+      component: CNavGroup,
+      name: 'Manage',
+      to: '/group',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '그룹 생성',
+          to: '/group/create',
+        },
+      ],
+    },
+    {
+      component: CNavTitle,
+      name: 'Analysis',
+    },
+    {
+      component: CNavGroup,
+      name: 'Survey Analysis',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '설문 분석',
+          to: '/analysis/user_survey_analysis',
+        },
+      ],
+    },
+    {
+      component: CNavTitle,
+      name: 'Admin',
+    },
+    {
+      component: CNavGroup,
+      name: 'Group Manage',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '그룹 리스트',
+          to: '/',
+        },
+      ],
+    },
+    {
+      component: CNavGroup,
+      name: 'Survey Manage',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '설문 카테고리 리스트',
+          to: '/category/category_list',
+        },
+      ],
+    },
+    {
+      component: CNavGroup,
+      name: 'Survey Analysis',
+      to: '/',
+      icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+      items: [
+        {
+          component: CNavItem,
+          name: '설문 분석',
+          to: '/analysis/ad_survey_analysis',
+        },
+      ],
+    },
+  ]
+
+
+
+
+  let dummy_data = [
+    {
+      "menuId": 1,
+      "menuName": "survey",
+      "menuUrl": null,
+      "menuIcon": null,
+      "menuList": [
+        {
+          "menuId": 101,
+          "menuName": "search",
+          "menuUrl": null,
+          "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+          "menuList": [
+            {
+              "menuId": 10101,
+              "menuName": "설문 검색",
+              "menuUrl": "/survey/search",
+              "menuIcon": null,
+              "menuList": []
+            }
+          ]
+        },
+        {
+          "menuId": 102,
+          "menuName": "create",
+          "menuUrl": null,
+          "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+          "menuList": [
+            {
+              "menuId": 10201,
+              "menuName": "설문 생성",
+              "menuUrl": "/survey",
+              "menuIcon": null,
+              "menuList": []
+            }
+          ]
+        },
+        {
+          "menuId": 103,
+          "menuName": "list",
+          "menuUrl": null,
+          "menuIcon": "<CIcon icon={cilSpreadsheet} customClassName=\"nav-icon\" />",
+          "menuList": [
+            {
+              "menuId": 10301,
+              "menuName": "참여 목록",
+              "menuUrl": "/survey/prtcpList",
+              "menuIcon": null,
+              "menuList": []
+            },
+            {
+              "menuId": 10302,
+              "menuName": "생성 목록",
+              "menuUrl": "/survey/makeList",
+              "menuIcon": null,
+              "menuList": []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+
+
+  useEffect(() =>{
+      dummy_data.map((large) => {
+
+          console.log('대분류: ',large.menuName)
+          setNavigation(
+            (navigation)=>[...navigation,{
+              component: CNavTitle,
+              name: large.menuName,
+            }])
+          console.log("여기야 !!!!!!!!!",navigation)
+    
+          large.menuList.map((middle) =>{
+            console.log("여기야 !!",navigation)
+            console.log('중분류: ', middle.menuName)
+            console.log('소분류들: ', middle.menuList)
+
+            let new_items = []
+
+            for (let i = 0; i < middle.menuList.length; i++) {
+              new_items.push(
+                {
+                  component: CNavItem,
+                  name: middle.menuList[i].menuName,
+                  to: middle.menuList[i].menuUrl,
+                }
+              )
+            }
+            setNavigation((navigation)=>[...navigation,{
+              component: CNavGroup,
+              name: middle.menuName,
+              to: '/',
+              icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
+              items: new_items
+            }])
+          }
+          )
+      }
+    )
+  }, []);
+
+
+  console.log("여기야 !!",navigation)
 
   return (
     <CSidebar
