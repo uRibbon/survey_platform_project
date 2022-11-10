fe0aff06916820249d7108be97ded7c6fefe0c61
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-11 04:24:29 +0900

    user 별 answer list 출력하는 서비스 생성

--- src/main/java/com/cloud/analysis/service/AnalysisService.java
+++ src/main/java/com/cloud/analysis/service/AnalysisService.java
@@ -1,4 +1,7 @@
 package com.cloud.analysis.service;
 
+import java.util.List;
+
 public interface AnalysisService {
+    List<Object> getUserAnswerList(Integer surId);
 }
--- src/main/java/com/cloud/analysis/service/AnalysisServiceImpl.java
+++ src/main/java/com/cloud/analysis/service/AnalysisServiceImpl.java
@@ -1,7 +1,32 @@
 package com.cloud.analysis.service;
 
+import com.cloud.analysis.entity.Answer;
+import com.cloud.analysis.entity.User;
+import com.cloud.analysis.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class AnalysisServiceImpl implements AnalysisService {
+    private final AnswerRepository answerRepository;
+
+    @Override
+    public List<Object> getUserAnswerList(Integer surId) {
+        List<Object> allList = new ArrayList<>();
+        List<User> userList = answerRepository.findAnswerBySurvey(surId);
+        userList.forEach(user -> {
+            List<Object> userAnswerList = new ArrayList<>();
+            userAnswerList.add(user);
+            List<Answer> answerList = answerRepository.findAnswerByUser(user.getUserId());
+            userAnswerList.add(answerList);
+            allList.add(userAnswerList);
+        });
+        return allList;
+    }
 }
