004423a5aa5fb4c9dfe7291b06bed3e082ef4c9c
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-10 22:31:03 +0900

    임시 user dto 구현(feign response data 역할)

---
+++ src/main/java/com/cloud/common/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.cloud.common.dto;
+
+import com.cloud.common.entity.Role;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserDTO {
+    private String userId;
+    private String userPwd;
+    private String name;
+    private String job;
+    private Integer age;
+    private Boolean gender;
+    private String phone;
+    private String mailAddr;
+    private Boolean isMailAccept;
+    private Boolean isPhoneAccept;
+    private Role role;
+    private String regId;
+    private LocalDateTime regDt;
+    private Boolean useYn;
+}
