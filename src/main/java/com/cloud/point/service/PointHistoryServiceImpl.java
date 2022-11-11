c6d3c5e80b26fe4dc8bd5b698a7f6985c0ecec53
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-11 19:35:03 +0900

    PointHistoryService 주석 추가

--- src/main/java/com/cloud/point/service/PointHistoryServiceImpl.java
+++ src/main/java/com/cloud/point/service/PointHistoryServiceImpl.java
@@ -2,16 +2,19 @@ package com.cloud.point.service;
 
 import com.cloud.point.entity.PointHistory;
 import com.cloud.point.repository.PointHistoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class PointHistoryServiceImpl implements PointHistoryService {
-
-    @Autowired
-    PointHistoryRepository pointHistoryRepository;
+    // repository 선언하기
+    private final PointHistoryRepository pointHistoryRepository;
 
     @Override
     public List<PointHistory> getAllPointHistoryList() {
