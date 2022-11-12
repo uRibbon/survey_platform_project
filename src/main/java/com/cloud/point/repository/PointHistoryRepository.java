b1f94e51631012aedcdb7e8060e752c51f90237c
서유리 <party22g@naver.com>
2022-11-12 15:57:21 +0900

    point 내역 조회 api 생성

--- src/main/java/com/cloud/point/repository/PointHistoryRepository.java
+++ src/main/java/com/cloud/point/repository/PointHistoryRepository.java
@@ -1,7 +1,6 @@
 package com.cloud.point.repository;
 
 import com.cloud.point.entity.PointHistory;
-import com.cloud.point.entity.PointType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Integer> {
