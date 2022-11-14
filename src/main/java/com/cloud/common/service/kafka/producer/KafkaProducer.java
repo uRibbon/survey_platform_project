e3c4101063a13acf31091745cf678f4e81300d15
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-15 01:14:12 +0900

    Kafka Producer 생성

---
+++ src/main/java/com/cloud/common/service/kafka/producer/KafkaProducer.java
@@ -0,0 +1,25 @@
+package com.cloud.common.service.kafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final String TOPIC = "common-topic";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(String.format("Produce message : %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
