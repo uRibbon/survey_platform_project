9412efe36dc1f82ead0a19c43f43149753ed47ea
solbiko <gsb0904@hanmail.net>
2022-11-14 18:52:38 +0900

    카프카 연동, producer, consumer 생성

---
+++ src/main/java/com/cloud/survey/service/kafka/producer/KafkaProducer.java
@@ -0,0 +1,19 @@
+package com.cloud.survey.service.kafka.producer;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer{
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        System.out.println(String.format("Produce message : %s", message));
+        this.kafkaTemplate.send(topic, message);
+    }
+}
