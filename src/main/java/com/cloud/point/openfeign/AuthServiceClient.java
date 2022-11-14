960a6745d595e5958b07f9a2e831d20fd8b28203
서유리 <party22g@naver.com>
2022-11-15 03:35:24 +0900

    feign authServiceClient, Config, Error 파일 생성

---
+++ src/main/java/com/cloud/point/openfeign/AuthServiceClient.java
@@ -0,0 +1,13 @@
+package com.cloud.point.openfeign;
+
+import com.cloud.point.dto.UserDTO;
+import org.apache.kafka.clients.admin.Config;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "auth-service", configuration = Config.class)
+public interface AuthServiceClient {
+
+    @GetMapping(value = "/v1/user/detail")
+    UserDTO getUserDetailInfo(@PathVariable String UserId);
+}
---
+++ src/main/java/com/cloud/point/openfeign/SurveyFeignError.java
@@ -0,0 +1,5 @@
+package com.cloud.point.openfeign;
+
+public class SurveyFeignError {
+
+}
