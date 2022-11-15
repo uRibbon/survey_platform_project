bc2c3616b13368066d1fd5530f0d73afc3ce8333
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-15 14:03:02 +0900

    question entity -> question id 변경, regDt 삭제 (엔티티에서 자동 생성됨)

--- src/main/java/com/cloud/survey/dto/AnswerDTO.java
+++ src/main/java/com/cloud/survey/dto/AnswerDTO.java
@@ -2,21 +2,22 @@ package com.cloud.survey.dto;
 
 import com.cloud.survey.entity.Question;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AnswerDTO {
 
     private int ansId;
-    private Question question;
+    private int queId;
     private int type;
     private String content;
     private String regId;
-    private LocalDateTime regDt;
 
 }
