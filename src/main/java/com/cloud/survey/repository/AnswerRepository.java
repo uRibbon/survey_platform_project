4fbcaac31face4b9071cd654bee1f60415f1a5bc
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-15 14:03:42 +0900

    deleteYn 값으로 삭제하는 쿼리 추가

--- src/main/java/com/cloud/survey/repository/AnswerRepository.java
+++ src/main/java/com/cloud/survey/repository/AnswerRepository.java
@@ -1,5 +1,6 @@
 package com.cloud.survey.repository;
 
+import com.cloud.survey.entity.Answer;
 import com.cloud.survey.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,12 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Map;
 
-public interface AnswerRepository extends JpaRepository<Question, Integer> {
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
     @Query(value = "select a.ans_id, a.que_id, a.type, a.content, qo.option_name, date_format(a.reg_dt, '%Y-%m-%d %H:%i:%S') reg_dt, a.reg_ip, q.sur_id, q.q_type, q.content question" +
             " from answer a left outer join question_option qo on a.que_id = qo.que_id and a.content = qo.option_order " +
             "left outer join question q on a.que_id = q.que_id WHERE a.reg_id =:userId and q.sur_id = :surId", nativeQuery=true)
     List<Map<String,Object>> findByRegIdAndSurId(@Param("userId") String userId, @Param("surId") int surId);
 
+    @Query("update Answer a set a.delYn = true where a.ansId = :ansId")
+    void updateDeleteYn(Integer ansId);
 
 }
