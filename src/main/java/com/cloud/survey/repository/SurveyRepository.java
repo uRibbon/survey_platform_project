e3c51bd99ffe7834ce37ebf0b2552ccb9c7ae538
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-16 20:28:46 +0900

    카테고리별 max 조회수 survey 출력 쿼리 생성

--- src/main/java/com/cloud/survey/repository/SurveyRepository.java
+++ src/main/java/com/cloud/survey/repository/SurveyRepository.java
@@ -4,11 +4,18 @@ import com.cloud.survey.entity.IsYn;
 import com.cloud.survey.entity.Survey;
 import com.cloud.survey.entity.SurveyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.Column;
 import java.util.List;
 
 public interface SurveyRepository extends JpaRepository<Survey, Integer> {
 
     List<Survey> findByStatusAndIsPrivateYn(SurveyStatus status, IsYn isPrivateYn);
     Survey findBySurId(int surId);
+
+    @Query("SELECT s FROM Survey s\n" +
+            "WHERE s.views = (SELECT MAX(s.views) FROM Survey s " +
+            "WHERE s.surveyCategory.surCatId = :surCatId)")
+    Survey findBestSurveyByCategory(Integer surCatId);
 }
