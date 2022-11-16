c7ecce74b724dfeba107a837eb34c96aa0390e15
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-16 15:58:10 +0900

    QuestionOption Entity 및 Repository 추가

---
+++ src/main/java/com/cloud/survey/entity/QuestionOption.java
@@ -0,0 +1,32 @@
+package com.cloud.survey.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionOption {
+    @Id
+    @Column(name = "que_opt_id", length = 10, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer queOptId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "que_id", nullable = false)
+    private Question question;
+
+    @Column(name = "option_order", length = 10, nullable = false)
+    private Integer optionOrder;
+
+    @Column(name = "option_name", length = 50, nullable = false)
+    private String optionName;
+
+
+}
