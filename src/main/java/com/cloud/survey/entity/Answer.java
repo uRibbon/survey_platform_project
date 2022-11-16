3fe33967959287b8c7ad32fe2ec3efcc7cc15fec
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-16 15:56:53 +0900

    답변 생성 및 수정일자 자동 생성을 위한 엔티티 리스너 어노테이션 추가

--- src/main/java/com/cloud/survey/entity/Answer.java
+++ src/main/java/com/cloud/survey/entity/Answer.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -17,50 +18,46 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Answer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ans_id")
+    @Column(name = "ans_id", nullable = false)
     private int ansId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "que_id", insertable=false, updatable=false)
+    @JoinColumn(name = "que_id", nullable = false)
     private Question question;
 
-    @NotNull
-    @Column(name = "type")
+    @Column(name = "type", nullable = false)
     private int type;
 
-    @NotNull
-    @Column(name = "content", length = 50)
+    @Column(name = "content", length = 50, nullable = false)
     private String content;
 
-    @NotNull
-    @Column(name = "reg_id")
+    @Column(name = "reg_id", nullable = false)
     private String regId;
 
-//    @NotNull
     @Column(name = "reg_ip", length = 50)
     private String regIp;
 
-    @NotNull
-    @Column(name = "reg_dt")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @CreatedDate
+    @Column(name = "reg_dt", nullable = false)
     private LocalDateTime regDt;
 
     @Column(name = "mod_id", length = 20)
     private String modId;
 
-    @Column(name = "mod_dt")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @LastModifiedDate
+    @Column(name = "mod_dt")
     private LocalDateTime modDt;
 
     @ColumnDefault("false")
+    @Column(name = "del_yn")
     private Boolean delYn;
-
 
     public void changeContent(String content) {
         this.content = content;
