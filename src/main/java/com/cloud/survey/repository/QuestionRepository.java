48d0d17e1ccf85e2376bb7a52714cd87f75afe81
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-16 15:59:20 +0900

    Question entity에서 survey를 외래키로 설정함에 따라 findBySurId 쿼리 수정

--- src/main/java/com/cloud/survey/repository/QuestionRepository.java
+++ src/main/java/com/cloud/survey/repository/QuestionRepository.java
@@ -5,10 +5,12 @@ import com.cloud.survey.entity.Question;
 import com.cloud.survey.entity.Survey;
 import com.cloud.survey.entity.SurveyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    @Query("select q from Question q where q.survey.surId = :surId")
     List<Question> findBySurId(int surId);
 
 
