e20caa41cea48c77765609072d1eba1519c6c95d
서유리 <party22g@naver.com>
2022-11-17 20:44:39 +0900

    pagenation에 맞게 GroupServiceImpl 수정

--- src/main/java/com/cloud/auth/service/GroupServiceImpl.java
+++ src/main/java/com/cloud/auth/service/GroupServiceImpl.java
@@ -1,11 +1,18 @@
 package com.cloud.auth.service;
 
+import com.cloud.auth.dto.GroupListDTO;
+import com.cloud.auth.dto.PageRequestDTO;
+import com.cloud.auth.dto.PageResultDTO;
 import com.cloud.auth.entity.Group;
 import com.cloud.auth.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +21,10 @@ public class GroupServiceImpl implements GroupService {
     private final GroupRepository groupRepository;
 
     @Override
-    public List<Group> getGroupList() {
-        List<Group> GroupList = groupRepository.findExistGroup();
-        return GroupList;
+    public PageResultDTO<GroupListDTO, Group> getGroupList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("groupId").descending());
+        Page<Group> groupListPage = groupRepository.findExistGroup(pageable);
+        Function<Group, GroupListDTO> fn = (groupList -> entityToDTO(groupList));
+        return new PageResultDTO<>(groupListPage, fn);
     }
 }
