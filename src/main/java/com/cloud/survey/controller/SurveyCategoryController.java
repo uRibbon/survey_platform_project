aa501549aa1a6216d73287be38e1e88a5140c5b1
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-17 17:08:52 +0900

    컨트롤러에 서비스 연동, 서베이 카테고리 조회 페이지네이션 추가

--- src/main/java/com/cloud/survey/controller/SurveyCategoryController.java
+++ src/main/java/com/cloud/survey/controller/SurveyCategoryController.java
@@ -1,7 +1,11 @@
 package com.cloud.survey.controller;
 
+import com.cloud.survey.dto.PageRequestDTO;
+import com.cloud.survey.dto.PageResultDTO;
+import com.cloud.survey.dto.SurveyCategoryDTO;
 import com.cloud.survey.entity.SurveyCategory;
 import com.cloud.survey.repository.SurveyCategoryRepository;
+import com.cloud.survey.service.SurveyCategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -16,24 +20,24 @@ import java.util.List;
 @RequestMapping(value="v1/survey/category")
 @RequiredArgsConstructor
 public class SurveyCategoryController {
-    private final SurveyCategoryRepository surveyCategoryRepository;
+    private final SurveyCategoryService surveyCategoryService;
 
     // pagination 미처리
-    @GetMapping("/read")
-    public ResponseEntity<List<SurveyCategory>> getCategoryList() {
-        List<SurveyCategory> surveyCategoryList = surveyCategoryRepository.findAll();
-        return new ResponseEntity<>(surveyCategoryList, HttpStatus.OK);
+    @GetMapping("/list")
+    public ResponseEntity<PageResultDTO<SurveyCategoryDTO, SurveyCategory>> getCategoryList(PageRequestDTO pageRequestDTO) {
+        PageResultDTO<SurveyCategoryDTO, SurveyCategory> categoryList = surveyCategoryService.getCategoryList(pageRequestDTO);
+        return new ResponseEntity<>(categoryList, HttpStatus.OK);
     }
 
     @PostMapping("/reg")
     public ResponseEntity<String> registerCategory(SurveyCategory surveyCategory) {
-        surveyCategoryRepository.save(surveyCategory);
+        surveyCategoryService.insertCategory(surveyCategory);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
     @GetMapping("/del/{surCatId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Integer surCatId) {
-        surveyCategoryRepository.deleteById(surCatId);
+        surveyCategoryService.deleteCategory(surCatId);
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
