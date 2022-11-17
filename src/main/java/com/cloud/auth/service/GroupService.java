a38f2e44cc5ad71f8f6904b980bdda24d4e3e82f
서유리 <party22g@naver.com>
2022-11-17 20:44:26 +0900

    pagenation에 맞게 GroupService 수정

--- src/main/java/com/cloud/auth/service/GroupService.java
+++ src/main/java/com/cloud/auth/service/GroupService.java
@@ -1,9 +1,42 @@
 package com.cloud.auth.service;
+import com.cloud.auth.dto.GroupListDTO;
+import com.cloud.auth.dto.PageRequestDTO;
+import com.cloud.auth.dto.PageResultDTO;
 import com.cloud.auth.entity.Group;
-import scala.Int;
 
 import java.util.List;
 
 public interface GroupService {
-    List<Group> getGroupList();
+//    List<Group> getGroupList();
+    PageResultDTO<GroupListDTO, Group> getGroupList(PageRequestDTO requestDTO);
+
+    default Group dtoToEntity(GroupListDTO dto) {
+        Group group = Group.builder()
+                .groupId(dto.getGroupId())
+                .groupName(dto.getGroupName())
+                .groupDescription(dto.getGroupDescription())
+                .groupCnt(dto.getGroupCnt())
+                .user(dto.getUser())
+                .regDt(dto.getRegDt())
+                .modId(dto.getModId())
+                .modDt(dto.getModDt())
+                .delYn(dto.getDelYn())
+                .build();
+        return group;
+    }
+
+    default GroupListDTO entityToDTO(Group group) {
+        GroupListDTO dto = GroupListDTO.builder()
+                .groupId(group.getGroupId())
+                .groupName(group.getGroupName())
+                .groupDescription(group.getGroupDescription())
+                .groupCnt(group.getGroupCnt())
+                .user(group.getUser())
+                .regDt(group.getRegDt())
+                .modId(group.getModId())
+                .modDt(group.getModDt())
+                .delYn(group.getDelYn())
+                .build();
+        return dto;
+    }
 }
