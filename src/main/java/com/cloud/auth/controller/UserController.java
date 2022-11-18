aea227feae6996e8394836808c54d372a01f2ee6
solbiko <gsb0904@hanmail.net>
2022-11-19 03:11:15 +0900

    keycloak, spring security, oauth2 resource server dependency 추가, application.yml 파일 수정, config 파일 추가, 테스트 api 추가

--- src/main/java/com/cloud/auth/controller/UserController.java
+++ src/main/java/com/cloud/auth/controller/UserController.java
@@ -2,18 +2,32 @@ package com.cloud.auth.controller;
 
 import com.cloud.auth.dto.UserDTO;
 import com.cloud.auth.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
 
 
 @RestController
 @RequestMapping(value="v1/user")
 public class UserController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserService userService;
+
+    @RolesAllowed({ "USER" })
+    @RequestMapping(value = "/keycloakTest", method = RequestMethod.GET)
+    public String keyclakTest(Principal principal, @RequestParam(value = "user_id") String UserId) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) principal; //jwt로 형변환
+        return token.getTokenAttributes().toString();
+    }
 
     @RequestMapping(value = "/detail", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> getUserDetail(@RequestParam(value = "user_id") String UserId) {
