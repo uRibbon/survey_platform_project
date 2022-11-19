797b57e469aa85f1b0d162ef269e7f475cb235ba
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-19 23:16:49 +0900

    답변 생성시 필요한 surId와 answerDTO 리스트를 담은 DTO

---
+++ src/main/java/com/cloud/survey/dto/AnswerResponseDTO.java
@@ -0,0 +1,17 @@
+package com.cloud.survey.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerResponseDTO {
+    Integer surId;
+    List<AnswerDTO> answerDTOList;
+}
