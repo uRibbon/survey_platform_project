4edd2ea7ea1779124f233f267ed0a73530f06827
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-21 02:46:24 +0900

    설문 상세 조회 및 설문 답변 생성

--- src/views/survey/create/ParticipateSurvey.js
+++ src/views/survey/create/ParticipateSurvey.js
@@ -8,18 +8,18 @@ import {
   CFormTextarea,
   CButton,
   CBadge,
+  CCallout, CForm,
 } from "@coreui/react";
 import axios from "axios";
 
 const ParticipateSurvey = () => {
-  const [surId, setSurId] = useState(2);
-  const [questionList, setQuestionList] = useState([]);
-  const [answerList, setAnswerList] = useState([{
-    queId: 0,
-    type: 0,
-    content: "",
-    regId: "yena"
-  }]);
+  const [surId, setSurId] = useState(2)
+  const [surInfo, setSurInfo] = useState([])
+  const [questionList, setQuestionList] = useState([])
+  const [content, setContent] = useState("")
+  const [answerList, setAnswerList] = useState([])
+
+
 
   axios.post("/survey-service/v1/survey/detail",
     {sur_id: surId},
@@ -27,45 +27,73 @@ const ParticipateSurvey = () => {
         'Content-Type': 'multipart/form-data'
       }}
   ).then((response) => {
+      setSurInfo(response.data.info)
       setQuestionList(response.data.question_list)
   })
 
-  const makeAnswer = (queId, event) => {
-    setAnswerList({
-      queId: queId,
-      type: 0,
-      content: event.currentTarget.value,
-      regId: "yena"
-    })
+  const makeAnswer = (e, queId) => {
+    const answerData = {
+      "queId": queId,
+      "type": 0,
+      "content": e.target.value,
+      "regId": "yena"
+    }
+    setAnswerList(answerList=>[...answerList, answerData])
   }
 
   const sendAnswer = () => {
-    const data = {
-      surId: surId,
-      answerDTOList: JSON.stringify(answerList)
-    }
-    console.log(data)
-    axios.post("/survey-service/v1/answer/reg",data
-
-    ).then((response) => {
+    axios.post("/survey-service/v1/answer/reg",
+      {
+        surId: surId,
+        answerDTOList: answerList
+      })
+      .then((response) => {
       window.location.reload("/#/survey/prtcpList");
     })
   }
 
   return(
-    console.log(questionList),
-    console.log(answerList),
     <>
-      <CBadge color="secondary" className="m-3 mb-0">기업</CBadge>
-      <h1 className="m-3">카카오 캐릭터 선호도 조사</h1>
+      <CBadge color="secondary" className="m-3 mb-0">{surInfo.categoryContent}</CBadge>
+      <h1 className="m-3">{surInfo.title}</h1>
+      <CCallout className="m-3" color="secondary">{surInfo.description}</CCallout>
+
       {questionList.map((question, index) => (
-        <CCard className="m-3" key={question.queId}>
-          <CCardHeader>질문{index+1}. {question.content}</CCardHeader>
-          <CCardBody>
-            <CFormTextarea onChange={(event) => makeAnswer(question.queId, event)}></CFormTextarea>
-          </CCardBody>
-        </CCard>
+          <CCard className="m-3" key={question.queId}>
+            <CCardHeader>질문{index+1}. {question.content}</CCardHeader>
+            <CCardBody>
+              <CFormTextarea name="content" onChange={(e) => makeAnswer(e,question.queId)}></CFormTextarea>
+
+              {question.qtype === "YN" && (
+                <>
+                  <CFormCheck type="radio" name="question4" id="question4" label="예" />
+                  <CFormCheck type="radio" name="question4" id="question4" label="아니오" />
+                </>
+              )}
+
+              {question.qtype === "Num" && (
+                <>
+                  <CFormCheck type="radio" name="question2" id="question2" label="1. 춘식이" />
+                  <CFormCheck type="radio" name="question2" id="question2" label="2. 라이언" />
+                  <CFormCheck type="radio" name="question2" id="question2" label="3. 어피치" />
+                  <CFormCheck type="radio" name="question2" id="question2" label="4. 포르도" />
+                </>
+              )}
+
+              {question.qtype === "Sub" && (
+                <CFormTextarea name="content" onChange={(e) => setContent(e.target.value)}></CFormTextarea>
+              )}
+
+              {question.qtype == "Grd" && (
+                <CFormRange min="0" max="100" step="10"/>
+              )}
+
+            </CCardBody>
+          </CCard>
       ))}
+
+      <CButton className="m-3" onClick={sendAnswer}>제출하기</CButton>
+
 
       {/*<CCard className="m-3">*/}
       {/*  <CCardHeader>질문1. 가장 좋아하는 카카오 캐릭터는?</CCardHeader>*/}
@@ -104,7 +132,7 @@ const ParticipateSurvey = () => {
       {/*    <CFormRange min="0" max="100" step="10"/>*/}
       {/*  </CCardBody>*/}
       {/*</CCard>*/}
-      <CButton className="m-3" onClick={sendAnswer}>제출하기</CButton>
+
     </>
 
   )
