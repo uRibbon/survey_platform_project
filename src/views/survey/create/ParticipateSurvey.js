632f131ca08b1b54c363f78ae229214e9c11f7ca
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-20 22:31:11 +0900

    설문 질문 조회(완성) 및 설문 답변 생성(미완성)

--- src/views/survey/create/ParticipateSurvey.js
+++ src/views/survey/create/ParticipateSurvey.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   CCard,
   CCardHeader,
@@ -9,50 +9,102 @@ import {
   CButton,
   CBadge,
 } from "@coreui/react";
+import axios from "axios";
 
 const ParticipateSurvey = () => {
+  const [surId, setSurId] = useState(2);
+  const [questionList, setQuestionList] = useState([]);
+  const [answerList, setAnswerList] = useState([{
+    queId: 0,
+    type: 0,
+    content: "",
+    regId: "yena"
+  }]);
+
+  axios.post("/survey-service/v1/survey/detail",
+    {sur_id: surId},
+    {headers: {
+        'Content-Type': 'multipart/form-data'
+      }}
+  ).then((response) => {
+      setQuestionList(response.data.question_list)
+  })
+
+  const makeAnswer = (queId, event) => {
+    setAnswerList({
+      queId: queId,
+      type: 0,
+      content: event.currentTarget.value,
+      regId: "yena"
+    })
+  }
+
+  const sendAnswer = () => {
+    const data = {
+      surId: surId,
+      answerDTOList: JSON.stringify(answerList)
+    }
+    console.log(data)
+    axios.post("/survey-service/v1/answer/reg",data
+
+    ).then((response) => {
+      window.location.reload("/#/survey/prtcpList");
+    })
+  }
+
   return(
+    console.log(questionList),
+    console.log(answerList),
     <>
       <CBadge color="secondary" className="m-3 mb-0">기업</CBadge>
       <h1 className="m-3">카카오 캐릭터 선호도 조사</h1>
-      <CCard className="m-3">
-        <CCardHeader>질문1. 가장 좋아하는 카카오 캐릭터는?</CCardHeader>
-        <CCardBody>
-          <CFormTextarea></CFormTextarea>
-        </CCardBody>
-      </CCard>
-      <CCard className="m-3">
-        <CCardHeader>질문2. 다음 중 가장 좋아하는 캐릭터는?</CCardHeader>
-        <CCardBody>
-          <CFormCheck type="radio" name="question2" id="question2" label="1. 춘식이" />
-          <CFormCheck type="radio" name="question2" id="question2" label="2. 라이언" />
-          <CFormCheck type="radio" name="question2" id="question2" label="3. 어피치" />
-          <CFormCheck type="radio" name="question2" id="question2" label="4. 포르도" />
-        </CCardBody>
-      </CCard>
-      <CCard className="m-3">
-        <CCardHeader>질문3. 싫어하는 캐릭터를 모두 골라주세요.</CCardHeader>
-        <CCardBody>
-          <CFormCheck type="checkbox" name="question3" id="question3" label="1. 춘식이" />
-          <CFormCheck type="checkbox" name="question3" id="question3" label="2. 라이언" />
-          <CFormCheck type="checkbox" name="question3" id="question3" label="3. 어피치" />
-          <CFormCheck type="checkbox" name="question3" id="question3" label="4. 포르도" />
-        </CCardBody>
-      </CCard>
-      <CCard className="m-3">
-        <CCardHeader>질문4. 춘식이를 좋아하나요?</CCardHeader>
-        <CCardBody>
-          <CFormCheck type="radio" name="question4" id="question4" label="예" />
-          <CFormCheck type="radio" name="question4" id="question4" label="아니오" />
-        </CCardBody>
-      </CCard>
-      <CCard className="m-3">
-        <CCardHeader>질문5. 춘식이를 얼마나 좋아하나요?</CCardHeader>
-        <CCardBody>
-          <CFormRange min="0" max="100" step="10"/>
-        </CCardBody>
-      </CCard>
-      <CButton className="m-3">제출하기</CButton>
+      {questionList.map((question, index) => (
+        <CCard className="m-3" key={question.queId}>
+          <CCardHeader>질문{index+1}. {question.content}</CCardHeader>
+          <CCardBody>
+            <CFormTextarea onChange={(event) => makeAnswer(question.queId, event)}></CFormTextarea>
+          </CCardBody>
+        </CCard>
+      ))}
+
+      {/*<CCard className="m-3">*/}
+      {/*  <CCardHeader>질문1. 가장 좋아하는 카카오 캐릭터는?</CCardHeader>*/}
+      {/*  <CCardBody>*/}
+      {/*    <CFormTextarea></CFormTextarea>*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
+      {/*<CCard className="m-3">*/}
+      {/*  <CCardHeader>질문2. 다음 중 가장 좋아하는 캐릭터는?</CCardHeader>*/}
+      {/*  <CCardBody>*/}
+      {/*    <CFormCheck type="radio" name="question2" id="question2" label="1. 춘식이" />*/}
+      {/*    <CFormCheck type="radio" name="question2" id="question2" label="2. 라이언" />*/}
+      {/*    <CFormCheck type="radio" name="question2" id="question2" label="3. 어피치" />*/}
+      {/*    <CFormCheck type="radio" name="question2" id="question2" label="4. 포르도" />*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
+      {/*<CCard className="m-3">*/}
+      {/*  <CCardHeader>질문3. 싫어하는 캐릭터를 모두 골라주세요.</CCardHeader>*/}
+      {/*  <CCardBody>*/}
+      {/*    <CFormCheck type="checkbox" name="question3" id="question3" label="1. 춘식이" />*/}
+      {/*    <CFormCheck type="checkbox" name="question3" id="question3" label="2. 라이언" />*/}
+      {/*    <CFormCheck type="checkbox" name="question3" id="question3" label="3. 어피치" />*/}
+      {/*    <CFormCheck type="checkbox" name="question3" id="question3" label="4. 포르도" />*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
+      {/*<CCard className="m-3">*/}
+      {/*  <CCardHeader>질문4. 춘식이를 좋아하나요?</CCardHeader>*/}
+      {/*  <CCardBody>*/}
+      {/*    <CFormCheck type="radio" name="question4" id="question4" label="예" />*/}
+      {/*    <CFormCheck type="radio" name="question4" id="question4" label="아니오" />*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
+      {/*<CCard className="m-3">*/}
+      {/*  <CCardHeader>질문5. 춘식이를 얼마나 좋아하나요?</CCardHeader>*/}
+      {/*  <CCardBody>*/}
+      {/*    <CFormRange min="0" max="100" step="10"/>*/}
+      {/*  </CCardBody>*/}
+      {/*</CCard>*/}
+      <CButton className="m-3" onClick={sendAnswer}>제출하기</CButton>
     </>
 
   )
