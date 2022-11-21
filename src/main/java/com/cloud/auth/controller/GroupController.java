e5bb6285238c0917f1432dd096a0d52307fc56ff
solbiko <gsb0904@hanmail.net>
2022-11-22 04:06:08 +0900

    그룹 컨트롤러 어노테이션 수정

--- src/main/java/com/cloud/auth/controller/GroupController.java
+++ src/main/java/com/cloud/auth/controller/GroupController.java
@@ -5,8 +5,8 @@ import com.cloud.auth.dto.PageRequestDTO;
 import com.cloud.auth.dto.PageResultDTO;
 import com.cloud.auth.entity.Group;
 import com.cloud.auth.service.GroupService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value="v1/user")
 @Slf4j
-@RequiredArgsConstructor
 public class GroupController {
-
-    private final GroupService groupService;
+    @Autowired
+    private  GroupService groupService;
 
     @PostMapping("/groupList")
     public ResponseEntity<PageResultDTO<GroupListDTO, Group>> getGroupList(PageRequestDTO pageRequestDTO) {
