74ce2e730e84a137e4410fff4b38eeff4e126c5a
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-21 13:46:18 +0900

    쿼리에 delYn=false 추가

--- src/main/java/com/cloud/survey/repository/AnswerRepository.java
+++ src/main/java/com/cloud/survey/repository/AnswerRepository.java
@@ -14,7 +14,7 @@ public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
     @Query(value = "select a.ans_id, a.que_id, a.type, a.content, qo.option_name, date_format(a.reg_dt, '%Y-%m-%d %H:%i:%S') reg_dt, a.reg_ip, q.sur_id, q.q_type, q.content question" +
             " from answer a left outer join question_option qo on a.que_id = qo.que_id and a.content = qo.option_order " +
-            "left outer join question q on a.que_id = q.que_id WHERE a.reg_id =:userId and q.sur_id = :surId", nativeQuery=true)
+            "left outer join question q on a.que_id = q.que_id WHERE a.reg_id =:userId and q.sur_id = :surId and a.del_yn=false", nativeQuery=true)
     List<Map<String,Object>> findByRegIdAndSurId(@Param("userId") String userId, @Param("surId") int surId);
 
     @Modifying
