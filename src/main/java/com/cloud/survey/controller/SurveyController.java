298de95f650869714dd4ebbc69b05d46316ccaf6
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-21 16:21:53 +0900

    설문 질문 조회 기능 surveyService -> questionService 이동

--- src/main/java/com/cloud/survey/controller/SurveyController.java
+++ src/main/java/com/cloud/survey/controller/SurveyController.java
@@ -4,12 +4,14 @@ import com.cloud.survey.dto.SurveyDTO;
 import com.cloud.survey.entity.IsYn;
 import com.cloud.survey.entity.Survey;
 import com.cloud.survey.entity.SurveyStatus;
+import com.cloud.survey.service.QuestionService;
 import com.cloud.survey.service.SurveyService;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -24,6 +26,9 @@ public class SurveyController {
     @Autowired
     private SurveyService surveyService;
 
+    @Autowired
+    private QuestionService questionService;
+
     // 설문 리스트 조회
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public ResponseEntity<List<SurveyDTO>> getSurveyList(@RequestParam (value = "status") SurveyStatus status,
@@ -32,12 +37,13 @@ public class SurveyController {
     }
 
     // 설문 상세정보, 질문 조회
+    @Transactional
     @RequestMapping(value = "/detail", method = RequestMethod.POST)
     public ResponseEntity<Map<String, Object>> getSurveyDetail(@RequestParam (value = "sur_id") int surId) {
 
         Map<String, Object> map = new HashMap<>();
         map.put("info", surveyService.getSurveyDetail(surId));
-        map.put("question_list", surveyService.getSurveyQuestion(surId));
+        map.put("question_list", questionService.getSurveyQuestion(surId));
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
