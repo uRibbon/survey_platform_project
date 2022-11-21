e6259a187b14278e4d98a2d945ab5ea9e882a4da
solbiko <gsb0904@hanmail.net>
2022-11-22 03:55:17 +0900

    분석 서비스 feign client 추가

---
+++ src/main/java/com/cloud/survey/openfeign/AnalysisServiceClient.java
@@ -0,0 +1,16 @@
+package com.cloud.survey.openfeign;
+
+import com.cloud.survey.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "analysis-client", url="localhost:9000/analysis-service/v1/analysis", configuration = FeignConfig.class)
+public interface AnalysisServiceClient {
+    @GetMapping(value = "/survey_analysis_info?sur_id={surId}")
+    List<Object> getTypeSubjectList(@PathVariable int surId);
+
+}
+
+
