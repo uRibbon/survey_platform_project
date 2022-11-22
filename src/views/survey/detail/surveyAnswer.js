7563af61def0762e50383959dba40b54c6d79cfc
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-22 21:39:36 +0900

    설문 상세 화면(작성한 답변 조회)

--- src/views/survey/detail/surveyAnswer.js
+++ src/views/survey/detail/surveyAnswer.js
@@ -17,7 +17,7 @@ const DetailInfo = (props) => {
 
   // 답변 정보 받아오기
   axios.post("/survey-service/v1/answer/list",
-    {regId: "yena", surId: props.surId},
+    {regId: "yena", surId: 2},
     {headers: {
         'Content-Type': 'multipart/form-data'
       }}
@@ -28,49 +28,76 @@ const DetailInfo = (props) => {
 
 
   return (
+    console.log(answerList),
     <div className='mb-3'>
         <CCol lg={12} className="text-start d-flex mb-1">
-          <CButton color="success" href="#" className="ms-auto" variant="outline">
-              excel download
-          </CButton>
+
         </CCol>
         <CAccordion alwaysOpen>
           {answerList.map((answer, index) => (
               <CAccordionItem itemKey={index+1} key={answer.queId}>
                 <CAccordionHeader>{index+1}. {answer.queContent} </CAccordionHeader>
                 <CAccordionBody>
-                  <CFormTextarea name="content" value={answer.ansContent} readOnly={true}></CFormTextarea>
-
-                  {answer.quetype === "YN" && (
+                  {answer.queType === "YN" && (
                     <>
                       {answer.optionList.map((option) => (
-                        <CFormCheck type="radio" name="content" key={option.queOptId} label={option.optionName} />
+                        <CFormCheck
+                          type="radio"
+                          name={answer.ansId}
+                          key={option.queOptId}
+                          label={option.optionName}
+                          checked={answer.ansContent === option.optionName?true:false}
+                          readOnly
+                        />
                       ))}
                     </>
                   )}
 
-                  {answer.quetype === "NumOnly" && (
+                  {answer.queType === "NumOnly" && (
                     <>
                       {answer.optionList.map((option) => (
-                        <CFormCheck type="radio" name="content" key={option.queOptId} label={option.optionName} />
+                        <CFormCheck
+                          type="radio"
+                          name={answer.ansId}
+                          key={option.queOptId}
+                          label={option.optionName}
+                          checked={answer.ansContent === option.optionName?true:false}
+                          readOnly
+                        />
                       ))}
                     </>
                   )}
 
-                  {answer.quetype === "NumMul" && (
+                  {answer.queType === "NumMul" && (
                     <>
                       {answer.optionList.map((option) => (
-                        <CFormCheck type="checkbox" name="content" key={option.queOptId} label={option.optionName} />
+                        <CFormCheck
+                          type="checkbox"
+                          name={answer.ansId}
+                          key={option.queOptId}
+                          label={option.optionName}
+                          checked={answer.ansContent === option.optionName?true:false}
+                          readOnly
+                        />
                       ))}
                     </>
                   )}
 
-                  {answer.quetype === "Sub" && (
-                    <CFormTextarea name="content"></CFormTextarea>
+                  {answer.queType === "Sub" && (
+                    <CFormTextarea
+                      name={answer.ansId}
+                      value={answer.ansContent}
+                      readOnly
+                    ></CFormTextarea>
                   )}
 
-                  {answer.quetype == "Grd" && (
-                    <CFormRange name="content" min="0" max="100" step="10"/>
+                  {answer.queType == "Grd" && (
+                    <CFormRange
+                      name={answer.ansId}
+                      min="0" max="100" step="10"
+                      value={answer.ansContent}
+                      readOnly
+                    />
                   )}
                 </CAccordionBody>
               </CAccordionItem>
