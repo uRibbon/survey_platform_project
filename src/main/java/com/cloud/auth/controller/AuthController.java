f93051176bed6075f831426688ea67dbc7694dd2
solbiko <gsb0904@hanmail.net>
2022-11-23 02:56:06 +0900

    키클록 회원가입, 로그인(토큰발급) 연동

---
+++ src/main/java/com/cloud/auth/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.cloud.auth.controller;
+
+import com.cloud.auth.dto.UserDTO;
+import com.cloud.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.representations.AccessTokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(value="v1/auth")
+@RequiredArgsConstructor
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity registerUser(@RequestBody UserDTO userDto) {
+        if(authService.existsByUsername(userDto.getUserId())) {
+            return ResponseEntity.ok("유저가 존재합니다.");
+        }
+
+        UserDTO result = authService.createUser(userDto);
+        return ResponseEntity.ok(result);
+    }
+
+    // 로그인
+    @PostMapping(path = "/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody UserDTO userDto) {
+
+        AccessTokenResponse response = authService.setAuth(userDto);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
---
+++ src/main/java/com/cloud/auth/service/AuthService.java
@@ -0,0 +1,113 @@
+package com.cloud.auth.service;
+
+import com.cloud.auth.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
+import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    @Value("${keycloak.auth-server-url}")
+    private String authServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    private final Keycloak keycloak;
+
+    public UserDTO createUser(UserDTO userDto) {
+
+        // 유저정보 세팅
+        UserRepresentation user = new UserRepresentation();
+        user.setEnabled(true);
+        user.setUsername(userDto.getUserId());
+
+        // Get realm
+        RealmResource realmResource = keycloak.realm(realm);
+        UsersResource usersResource = realmResource.users();
+
+        Response response = usersResource.create(user);
+        if(response.getStatus() == 201) {
+
+            String userId = CreatedResponseUtil.getCreatedId(response);
+
+            // create password credential
+            CredentialRepresentation passwordCred = new CredentialRepresentation();
+            passwordCred.setTemporary(false);
+            passwordCred.setType(CredentialRepresentation.PASSWORD);
+            passwordCred.setValue(userDto.getUserPwd());
+            log.info("Created userId {}", userId);
+            UserResource userResource = usersResource.get(userId);
+
+            // Set password credential
+            userResource.resetPassword(passwordCred);
+
+            // role 세팅
+            ClientRepresentation clientRep = realmResource.clients().findByClientId(clientId).get(0);
+            RoleRepresentation clientRoleRep = realmResource.clients().get(clientRep.getId()).roles().get(userDto.getUserRole().getCode()).toRepresentation();
+            userResource.roles().clientLevel(clientRep.getId()).add(Arrays.asList(clientRoleRep));
+
+        }
+
+        userDto.setStatus(response.getStatus());
+//        userDto.setStatusInfo(response.getStatusInfo().toString());
+
+        return userDto;
+    }
+
+    public AccessTokenResponse setAuth(UserDTO userDto) {
+        Map<String, Object> clientCredentials = new HashMap<>();
+        clientCredentials.put("secret", clientSecret);
+        clientCredentials.put("grant_type", "password");
+
+        Configuration configuration =
+                new Configuration(authServerUrl, realm, clientId, clientCredentials, null);
+        AuthzClient authzClient = AuthzClient.create(configuration);
+
+        AccessTokenResponse response =
+                authzClient.obtainAccessToken(userDto.getUserId(), userDto.getUserPwd());
+
+        return response;
+    }
+
+    /*
+     *  사용자 존재하는지 체크
+     * */
+    public boolean existsByUsername(String userName) {
+
+        List<UserRepresentation> search = keycloak.realm(realm).users().search(userName);
+        if(search.size() > 0){
+            log.debug("search : {}", search.get(0).getUsername());
+            return true;
+        }
+        return false;
+    }
+}
