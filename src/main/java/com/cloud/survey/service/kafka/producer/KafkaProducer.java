eb56cfc9f581de82705bd0d973c696eb4bd3668d
solbiko <gsb0904@hanmail.net>
2022-11-22 19:06:27 +0900

    카프카 프로듀서 수정

--- src/main/java/com/cloud/survey/service/kafka/producer/KafkaProducer.java
+++ src/main/java/com/cloud/survey/service/kafka/producer/KafkaProducer.java
@@ -6,23 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer{
 
-    private final KafkaTemplate<String, String> kafkaTemplateStr;
-    private final KafkaTemplate<String, Map<String,String>> kafkaTemplateStrMap;
     private final KafkaTemplate<String, Map<String,Object>> kafkaTemplateObjectMap;
 
-    public void sendString(String topic, String str) {
-        this.kafkaTemplateStr.send(topic, str);
-    }
-    public void sendStrMap(String topic, Map<String,String> map) {
-        this.kafkaTemplateStrMap.send(topic, map);
-    }
     public void sendObjetMap(String topic, Map<String,Object> map) {
         this.kafkaTemplateObjectMap.send(topic, map);
     }
