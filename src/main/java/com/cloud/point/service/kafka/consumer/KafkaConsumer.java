d7021a0c38ed0f301685bbba65987475475cbbc5
서유리 <party22g@naver.com>
2022-11-24 02:06:52 +0900

    kafka consumer 생성(설문 답변 토픽 받아오기)

--- src/main/java/com/cloud/point/service/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/cloud/point/service/kafka/consumer/KafkaConsumer.java
@@ -1,10 +1,9 @@
 package com.cloud.point.service.kafka.consumer;
 
-import com.cloud.point.service.UserPointService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Log4j2
@@ -15,12 +14,21 @@ public class KafkaConsumer {
 
     @KafkaListener(topics = "ANSWER_POINT", groupId = "survey_platform")
     public void consume(Map<String,Object> accruePointMap) {
-        Object user_id = accruePointMap.get("user_id");
-        Object surId = accruePointMap.get("sur_id");
-        Object type = accruePointMap.get("type");
+        Object map2 = accruePointMap.get(("accrue_point_map"));
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        System.out.println("user_id " + user_id);
-        System.out.println("sur_id " + surId);
-        System.out.println("type " + type);
+        Map result = objectMapper.convertValue(map2, Map.class);
+        result.get("user_id");
+
+//        String userId = accruePointMap.get()
+//        String surId = accruePointMap.get("accrue_point_map").
+//        String type = accruePointMap.get("accrue_point_map")
+//
+//        System.out.println("user_id: " + userId);
+//        System.out.println("sur_id: " + surId);
+//        System.out.println("type: " + type);
     }
+
+
+
 }
