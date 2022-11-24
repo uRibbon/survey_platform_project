d5b64f12d649ab0c61ace6ceeb98f31e1538251d
solbiko <gsb0904@hanmail.net>
2022-11-25 02:35:23 +0900

    리덕스 사가 추가, 미들웨어 추가

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import store from './store'
+// import store from './store'
+
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import {tempSetUser, check} from './modules/user'
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+  );
+
+sagaMiddleware.run(rootSaga);
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
