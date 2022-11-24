5b88cbce8f206ba78d2220e3dcd7d0dc6aa43754
solbiko <gsb0904@hanmail.net>
2022-11-24 16:28:16 +0900

    설문 생성 페이지 수정

--- src/views/survey/create/CreateSurvey.js
+++ src/views/survey/create/CreateSurvey.js
@@ -48,6 +48,7 @@ const Charts = (props) => {
 
   const [surInfo, setSurInfo] = useState([])
 
+/*
   axios.post("/survey-service/v1/survey/detail",
     {sur_id: 2},
     {headers: {
@@ -56,55 +57,61 @@ const Charts = (props) => {
   ).then((response) => {
     setSurInfo(response.data.info)
   })
-
+*/
   return (
     console.log(surInfo),
     <>
-      <CCard className="mb-3">
-        <CCardHeader>설문 설정</CCardHeader>
+     <CCard className="mb-3">
+        <CCardHeader>설문 생성<small> 설문을 생성 할 수 있습니다.</small>
+        </CCardHeader>
         <CCardBody>
-          <CForm className="row g-3">
-            <CCol xs={6}>
-              <CFormInput label="설문 이름" placeholder="설문 이름을 입력하세요" value={surInfo.title}/>
-            </CCol>
-            <CCol xs={6}>
-              <CFormSelect label="설문 분류 선택" value={surInfo.categoryId}>
-                <option value={1}>학교</option>
-                <option value={2}>기업</option>
-                <option value={3}>연애</option>
-                <option value={4}>사업</option>
-                <option value={5}>취미</option>
-              </CFormSelect>
-            </CCol>
-            <CCol xs={3}>
-              <CFormSelect label="로그인 여부" value={surInfo.isLoginYn}>
-                <option value="Y">로그인 필수</option>
-                <option value="N">로그인 필요 없음</option>
-              </CFormSelect>
-            </CCol>
-            <CCol xs={3}>
-              <CFormSelect label="설문 공개 선택" value={surInfo.isPrivateYn}>
-                <option value="N">전체 공개</option>
-                <option value="2">그룹 공개</option>
-                <option value="Y">비공개</option>
-              </CFormSelect>
-            </CCol>
-            <CCol xs={3}>
-              <CFormSelect label="답변 수정 여부" value={surInfo.isModifyYn}>
-                <option value="Y">수정 가능</option>
-                <option value="N">수정 불가능</option>
-              </CFormSelect>
-            </CCol>
-            <CCol xs={3}>
-              <CFormSelect label="익명 여부 선택" value={surInfo.isAnnoyYn}>
-                <option value="Y">익명 가능</option>
-                <option value="N">수정 불가능</option>
-              </CFormSelect>
-            </CCol>
-            <CCol xs={12}>
-              <CFormInput label="설문 상세" placeholder="설문 설명을 입력하세요" value={surInfo.description}/>
-            </CCol>
-          </CForm>
+          <CCard className="mb-3">
+            <CCardHeader>설문 설정</CCardHeader>
+            <CCardBody>
+              <CForm className="row g-3">
+                <CCol xs={6}>
+                  <CFormInput label="설문 이름" placeholder="설문 이름을 입력하세요" value={surInfo.title}/>
+                </CCol>
+                <CCol xs={6}>
+                  <CFormSelect label="설문 분류 선택" value={surInfo.categoryId}>
+                    <option value={1}>학교</option>
+                    <option value={2}>기업</option>
+                    <option value={3}>연애</option>
+                    <option value={4}>사업</option>
+                    <option value={5}>취미</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol xs={3}>
+                  <CFormSelect label="로그인 여부" value={surInfo.isLoginYn}>
+                    <option value="Y">로그인 필수</option>
+                    <option value="N">로그인 필요 없음</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol xs={3}>
+                  <CFormSelect label="설문 공개 선택" value={surInfo.isPrivateYn}>
+                    <option value="N">전체 공개</option>
+                    <option value="2">그룹 공개</option>
+                    <option value="Y">비공개</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol xs={3}>
+                  <CFormSelect label="답변 수정 여부" value={surInfo.isModifyYn}>
+                    <option value="Y">수정 가능</option>
+                    <option value="N">수정 불가능</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol xs={3}>
+                  <CFormSelect label="익명 여부 선택" value={surInfo.isAnnoyYn}>
+                    <option value="Y">익명 가능</option>
+                    <option value="N">수정 불가능</option>
+                  </CFormSelect>
+                </CCol>
+                <CCol xs={12}>
+                  <CFormInput label="설문 상세" placeholder="설문 설명을 입력하세요" value={surInfo.description}/>
+                </CCol>
+              </CForm>
+            </CCardBody>
+          </CCard>
         </CCardBody>
       </CCard>
 
