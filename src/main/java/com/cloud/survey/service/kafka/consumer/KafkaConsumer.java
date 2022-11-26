425755fd584586a7952caa70ce7fdd21b47a397d
solbiko <gsb0904@hanmail.net>
2022-11-26 14:44:31 +0900

    테스트 컨슈머 주석처리

--- src/main/java/com/cloud/survey/service/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/cloud/survey/service/kafka/consumer/KafkaConsumer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "test", groupId = "survey_platform")
+//    @KafkaListener(topics = "test", groupId = "survey_platform")
     public void consume(String message) throws IOException {
         System.out.println(String.format("Consumed message : %s", message));
     }
