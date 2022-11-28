52ec05892a51e1c47082f72e0b8297b5c7b14811
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-28 18:58:21 +0900

    권한 수정

--- src/main/java/com/cloud/common/entity/UserRole.java
+++ src/main/java/com/cloud/common/entity/UserRole.java
@@ -1,6 +1,18 @@
 package com.cloud.common.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum UserRole {
-    ADMIN,
-    MEMBER
+
+    ADMIN("관리자"),
+    USER("사용자"),
+    ALL("전체");
+
+    private final String title;
+    public String getCode() {
+        return name();
+    }
 }
