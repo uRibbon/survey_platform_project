8aca38f54240f0bf1f9bfe839b9dcc11f725b1e8
신예나 <48668024+shinyena@users.noreply.github.com>
2022-11-29 21:49:37 +0900

    header로 access token 전송, apiconfig 사용, dummy data 삭제

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -10,6 +10,7 @@ import {
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 import axios from 'axios';
+import apiConfig from "../lib/apiConfig";
 
 
 const AppSidebar = () => {
@@ -17,161 +18,12 @@ const AppSidebar = () => {
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
   const [navigation, setNavigation] = useState([])
-
-  // let DUMMY_DATA = [
-  //   {
-  //     component: CNavTitle,
-  //     name: 'Survey',
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Search',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '설문검색',
-  //         to: '/survey/search',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Create',
-  //     to: '/survey',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '설문생성',
-  //         to: '/survey',
-  //       },
-  //       {
-  //         component: CNavItem,
-  //         name: '설문참여',
-  //         to: '/survey/ParticipateSurvey',
-  //       },
-  //       {
-  //         component: CNavItem,
-  //         name: '설문수정',
-  //         to: '/survey/ModifySurvey',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'List',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '참여목록',
-  //         to: '/survey/prtcpList',
-  //       },
-  //       {
-  //         component: CNavItem,
-  //         name: '작성목록',
-  //         to: '/survey/makeList',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavTitle,
-  //     name: 'Group',
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'List',
-  //     to: '/group',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '그룹 목록',
-  //         to: '/group/list',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Manage',
-  //     to: '/group',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '그룹 생성',
-  //         to: '/group/create',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavTitle,
-  //     name: 'Analysis',
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Survey Analysis',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '설문 분석',
-  //         to: '/analysis/user_survey_analysis',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavTitle,
-  //     name: 'Admin',
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Group Manage',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '그룹 리스트',
-  //         to: '/',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Survey Manage',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '설문 카테고리 리스트',
-  //         to: '/category/category_list',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     component: CNavGroup,
-  //     name: 'Survey Analysis',
-  //     to: '/',
-  //     icon: <CIcon icon={cilSpreadsheet} customClassName="nav-icon" />,
-  //     items: [
-  //       {
-  //         component: CNavItem,
-  //         name: '설문 분석',
-  //         to: '/analysis/ad_survey_analysis',
-  //       },
-  //     ],
-  //   },
-  // ]
+  const { user } = useSelector(({user})=> ({user:user.user}));
+  const accessToken = user.token.access_token;
 
   useEffect(async () =>{
       let test = []
-      const response = await axios.get('/common-service/v1/common/menu')
+      const response = await axios.get(apiConfig.menuList, {headers: {'Authorization': 'Bearer ' + accessToken }})
       test = response.data
       test.map((large) => {
           setNavigation(
