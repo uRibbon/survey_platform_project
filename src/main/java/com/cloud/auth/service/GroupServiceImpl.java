37423a7187e6e72afaea7977e4daa82ae3b37953
서유리 <party22g@naver.com>
2022-11-30 23:27:45 +0900

    GroupListDTO 삭제 후 GroupDTO로 수정

--- src/main/java/com/cloud/auth/service/GroupServiceImpl.java
+++ src/main/java/com/cloud/auth/service/GroupServiceImpl.java
@@ -1,12 +1,10 @@
 package com.cloud.auth.service;
 
 import com.cloud.auth.dto.GroupDTO;
-import com.cloud.auth.dto.GroupListDTO;
 import com.cloud.auth.dto.PageRequestDTO;
 import com.cloud.auth.dto.PageResultDTO;
 import com.cloud.auth.entity.Group;
 import com.cloud.auth.entity.User;
-import com.cloud.auth.entity.UserGroup;
 import com.cloud.auth.repository.GroupRepository;
 import com.cloud.auth.repository.UserGroupRepository;
 import com.cloud.auth.repository.UserRepository;
@@ -16,8 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -62,11 +58,9 @@ public class GroupServiceImpl implements GroupService {
 
     // 그룹 생성
     @Override
-    public void insertGroup(GroupListDTO groupListDTO) {
-        User user = userRepository.findByUserId(groupListDTO.getRegId());
-        Group group = dtoToEntity(groupListDTO, user);
+    public void insertGroup(GroupDTO groupDTO) {
+        User user = userRepository.findByUserId(groupDTO.getRegId());
+        Group group = dtoToEntity(groupDTO, user);
         groupRepository.save(group);
     }
-
-
 }
