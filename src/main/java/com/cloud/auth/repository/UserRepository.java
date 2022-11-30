904cd3ea6d0898acdf897ff098ad625c776b50c3
서유리 <party22g@naver.com>
2022-11-30 13:37:57 +0900

    그룹 참여자 api 생성

--- src/main/java/com/cloud/auth/repository/UserRepository.java
+++ src/main/java/com/cloud/auth/repository/UserRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
     User findByUserId(@Param("userId") String UserId);
 }
