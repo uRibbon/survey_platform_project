7ccf988fda56389f98354ce38828f8ec87f7df8a
서유리 <party22g@naver.com>
2022-12-02 01:28:36 +0900

    GroupDTO 수정 && GroupService entityToDTO 수정

--- src/main/java/com/cloud/auth/dto/GroupDTO.java
+++ src/main/java/com/cloud/auth/dto/GroupDTO.java
@@ -3,8 +3,10 @@ package com.cloud.auth.dto;
 import com.cloud.auth.entity.DelYn;
 import com.cloud.auth.entity.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,6 +25,7 @@ public class GroupDTO {
 
     private Integer groupCnt;
 
+    // 개설자 정보
     private String regId;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
@@ -36,13 +39,6 @@ public class GroupDTO {
     private List<User> prtcpList;
 
     private DelYn delYn;
-
-    // 아래 : 그룹 참여자 정보
-    private String userId;
-
-    private String userName;
-
-    private String userMailAddr;
 
     public void addPrtcpList(User user) {
         prtcpList.add(user);
