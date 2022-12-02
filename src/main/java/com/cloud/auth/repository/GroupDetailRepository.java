03ec3b0c0d150ed8df314ed25b4adabb8cf4637f
서유리 <party22g@naver.com>
2022-12-02 10:53:17 +0900

    GroupDetail 관련 파일 삭제

--- src/main/java/com/cloud/auth/repository/GroupDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.auth.repository;
-
-import com.cloud.auth.entity.GroupDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface GroupDetailRepository extends JpaRepository<GroupDetail, Integer> {
-    GroupDetail findByGroupId(Integer groupId);
-}
